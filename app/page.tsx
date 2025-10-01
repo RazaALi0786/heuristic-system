7efec3fbfeeb63e5e1c11c7275ecb644375d7ec1
Shivansh Tiwari <shiv4nshtiw4ri@gmail.com>
2025-10-01 15:52:35 +0530

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1 className="text-5xl font-bold text-orange-600">Heuristic System</h1>
       <p className="mt-6 text-xl max-w-2xl text-gray-700">
         Empowering Business Success Through Innovative Technology Solutions and
-        Expert Consulting.
+        Expert Ali.
       </p>
       <button className="mt-8 bg-orange-600 text-white px-6 py-3 rounded-lg hover:bg-orange-700 cursor-pointer">
         View Jobs
