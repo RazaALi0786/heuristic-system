ac51c0b6d7ed81a9be625ed2b77aabf5a0dc44c7
Syed Raza Ali <razaali26296@gmail.com>
2025-10-12 11:52:29 +0530

    update

--- app/api/contact/[id]/route.ts
+++ app/api/contact/[id]/route.ts
@@ -1,17 +1,24 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/lib/mongodb";
 import Contact from "@/lib/models/Contact";
 
 export async function DELETE(
-  req: NextRequest,
+  req: Request,
   { params }: { params: { id: string } }
 ) {
+  const { id } = params;
+
+  if (!id) {
+    return NextResponse.json(
+      { success: false, message: "ID is required" },
+      { status: 400 }
+    );
+  }
+
+  await connectToDatabase();
+
   try {
-    await connectToDatabase();
-
-    const { id } = params;
     await Contact.findByIdAndDelete(id);
-
     return NextResponse.json({ success: true, message: "Contact deleted" });
   } catch (error) {
     console.error("Delete failed:", error);
